a0a3f59762493467b56d5c809bd5cc8c8ef55ff9
Einstein <61317429+egitim360@users.noreply.github.com>
2023-05-01 15:58:52 +0300

    Add files via upload

---
+++ PHPMailTemplate/success.php
@@ -0,0 +1,215 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+
+<head>
+  <title>Eğitim360 - Yeni Nesil Eğitim Platformu</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+</head>
+
+<body bgcolor="#207efa" style="margin: 0px; width:100% !important; -webkit-text-size-adjust:none; padding-top: 50px; padding-bottom: 50px;">
+  <table width="750" align="center" border="0" cellpadding="0" cellspacing="0" style=" font-family:Helvetica,Roboto,Segoe UI,sans-serif; background-color: white; border-radius: 15px;">
+    <tr>
+      <td>
+        <table width="750" align="center" border="0" cellpadding="0" cellspacing="0" style="background-color: #f2f1f7; border-radius: 15px;">
+          <tr>
+            <td>
+              <table style="padding-left: 40px;" border="0" cellpadding="0" cellspacing="0">
+                <tr>
+                  <td>
+                    <a href="http://www.egitim360.cloud" target="_blank" rel="noopener noreferrer" data-auth="NotApplicable">
+                      <img data-imagetype="" src="http://www.egitim360.cloud/assets/mail/logo.png" alt="eğitim360" width="" border="0" style="display:block; outline:none; border-collapse:collapse;">
+                    </a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <table>
+                      <tr>
+                        <td style="color:#ac6fdd;
+                                                font-weight:700;
+                                                font-size:25px;
+                                                line-height:1.7;
+                                                padding-top: 50px;
+                                                padding-bottom: 10px;
+                                                ">
+                          Kaydınız Alınmıştır
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style="font-size: 56px;
+                                                background-color: #207efa;
+                                                color:white;
+                                                padding: 0 10px;
+                                                ">
+                          Teşekkürler
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </td>
+            <td>
+              <table align="center" border="0" cellpadding="0" cellspacing="0">
+                <tr>
+                  <td>
+                    <img data-imagetype="" src="http://www.egitim360.cloud/assets/mail/header-img.png" alt="eğitim360" width="" border="0" style="display:block; margin-top: -25px;">
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>
+        <table align="center" border="0" cellpadding="0" cellspacing="0" style="padding-bottom: 30px;">
+          <tr>
+            <td>
+              <table align="center" border="0" cellpadding="0" cellspacing="0" style="text-align: center;">
+                <tr>
+                  <td>
+                    <img data-imagetype="" src="http://www.egitim360.cloud/assets/mail/profile-logo.png" alt="eğitim360" width="" border="0" style="display:block; margin: 30px auto;">
+                  </td>
+                </tr>
+                <tr>
+                  <td style="font-size: 25px; font-weight: 500; padding-bottom: 20px;">{adSoyad}</td>
+                </tr>
+                <tr>
+                  <td style="font-size: 20px; font-weight: 300; padding-bottom: 10px; line-height: 1.7;">
+                    <span style="color: #207efa;">Ims.eğitim360.cloud</span> üzerinden oluşturmuş olduğunuz <br>
+                    <span style="color:#ac6fdd">LMS talebiniz</span> tarafımıza ulaşmıştır.<br>
+                    En kısa zamanda tarafınıza <span style="color: #207efa;">dönüş sağlanacaktır.</span>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>
+        <table align="center" border="0" cellpadding="0" cellspacing="0">
+          <tr>
+            <td>
+              <table align="center" border="0" cellpadding="0" cellspacing="0">
+                <tr>
+                  <td>
+                    <img data-imagetype="" src="http://www.egitim360.cloud/assets/mail/line-break-top.jpg" alt="" width="750" border="0" style="display:block;">
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <table align="center" border="0" cellpadding="0" cellspacing="0" style="padding: 60px 30px;">
+                      <tr>
+                        <td>
+                          <table align="center" border="0" cellpadding="0" cellspacing="0">
+                            <tr>
+                              <td style="font-size: 28px; font-weight: 300;
+                                                        padding-bottom: 30px; line-height: 1.2; color:#ac6fdd;
+                                                        padding-top: 40px;">
+                                İşte,<br>Kendimizden<br>Öykündüğümüz<br>Hikayemiz
+                              </td>
+                            </tr>
+                            <tr>
+                              <td style="font-size: 14px; font-weight: 300;
+                                                        padding-bottom: 30px; line-height: 1.2; color:#000000 ;">
+                                Aydınlıktan beslenen bir hikayenin<br>gerçeğe dönüşmesine şahidiz.
+                              </td>
+                            </tr>
+                          </table>
+                        </td>
+                        <td>
+                          <a href="https://www.einstein.com.tr/#tanitim-videosu" target="_blank" rel="noopener noreferrer" data-auth="NotApplicable">
+                            <img data-imagetype="" src="http://www.egitim360.cloud/assets/mail/main-img.png" alt="eğitim360" width="100%" border="0" style="display:block;">
+                          </a>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <img data-imagetype="" src="http://www.egitim360.cloud/assets/mail/line-break-bottom.jpg" alt="" width="750" border="0" style="display:block;">
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>
+        <table border="0" cellpadding="0" cellspacing="0">
+          <tr>
+            <td>
+              <a href="https://katalog.egitim360.cloud/" target="_blank" rel="noopener noreferrer" data-auth="NotApplicable">
+                <img data-imagetype="" src="http://www.egitim360.cloud/assets/mail/e-katalog.jpg" alt="eğitim360" width="" border="0" style="display:block; margin: 0 auto;">
+              </a>
+            </td>
+          </tr>
+        </table>
+
+      </td>
+    </tr>
+  </table>
+  <table align="center" border="0" cellpadding="0" cellspacing="0" width="750" style="padding-top: 20px;">
+    <tr>
+      <td>
+        <table border="0" cellpadding="0" cellspacing="0" width="750">
+          <tr>
+            <td>
+              <table border="0" cellpadding="0" cellspacing="0" style="background-color:#c68ed5;">
+                <tr>
+                  <td>
+                    <a href="https://www.einstein.com.tr/kullanim-kosullari" target="_blank" rel="noopener noreferrer" data-auth="NotApplicable">
+                      <img data-imagetype="" src="http://www.egitim360.cloud/assets/mail/kullanım-kos.jpg" alt="eğitim360" width="" border="0" style="display:block; margin: 0 auto;">
+                    </a>
+                  </td>
+                  <td style="width:1px; height: 10px; background-color: #c68ed5;">
+                  </td>
+                  <td>
+                    <a href="https://www.einstein.com.tr/cerez-politikasi" target="_blank" rel="noopener noreferrer" data-auth="NotApplicable">
+                      <img data-imagetype="" src="http://www.egitim360.cloud/assets/mail/gizlilik.jpg" alt="eğitim360" width="" border="0" style="display:block; margin: 0 auto;">
+                    </a>
+                  </td>
+                </tr>
+              </table>
+            </td>
+            <td>
+              <table align="right" border="0" cellspacing="0" cellpadding="0" style="background-color: #515266; height: 93px;">
+
+                <tr>
+                  <td style="height: 93px;">
+                    <table style="height: 93px;">
+                      <tr>
+                        <td align="right" valign="middle" style="width:145px; height:87px">
+                        <td align="right" valign="middle" style="width:40px; height:25px;">
+                            <a href="https://www.instagram.com/einsteinelearning/" style="display:inline-block; text-decoration:none;">
+                            <img class="social-icons" src="http://www.egitim360.cloud/assets/mail/instagram.png" width="25" height="25" alt="Instagram" style="display:inline-block; border:none;"></a>
+                        </td>
+                        <td align="right" valign="middle" style="width:40px; height:25px;">
+                            <a href="https://facebook.com/einsteinelearning" style="display:inline-block; text-decoration:none;">
+                            <img class="social-icons" src="http://www.egitim360.cloud/assets/mail/facebook.png" width="25" height="25" alt="Facebook" style="display:inline-block; border:none;"></a>
+                        </td>
+                        <td align="right" valign="middle" style="width:40px; height:25px;"><a href="https://twitter.com/Einsteincomtr" style="display:inline-block; text-decoration:none;">
+                            <img class="social-icons" src="http://www.egitim360.cloud/assets/mail/twitter.png" width="25" height="25" alt="Twitter" style="display:inline-block; border:none;"></a>
+                        </td>
+                        <td align="right" valign="middle" style="width:40px; height:25px;"><a href="https://www.youtube.com/channel/UC-XsYMNLRlNYy2xNGEqQrHw?view_as=subscriber" style="display:inline-block; text-decoration:none;">
+                            <img class="social-icons" src="http://www.egitim360.cloud/assets/mail/youtube.png" width="25" height="25" alt="Youtube" style="display:inline-block; border:none;"></a>
+                        </td>
+                        <td align="right" valign="middle" style="width:40px; height:25px;"><a href="https://vimeo.com/einsteincomtr" style="display:inline-block; text-decoration:none;">
+                            <img class="social-icons" src="http://www.egitim360.cloud/assets/mail/vimeo.png" width="25" height="25" alt="Vimeo" style="display:inline-block; border:none;"></a>
+                        </td>
+                        <td align="right" valign="middle" style="width:40px; height:25px;"><a href="https://tr.linkedin.com/company/-e-instein" style="display:inline-block; text-decoration:none;">
+                            <img class="social-icons" src="http://www.egitim360.cloud/assets/mail/linkedin.png" width="25" height="25" alt="Linledin" style="display:inline-block; border:none;"></a>
+                        </td>
+                        <td align="right" valign="middle" width="30"></td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+
+              </table>
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table>
+</body>
+
+</html>
